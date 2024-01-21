54f1711e0db19193b4777a462ca6712171734333
wbrunovieira <wbrunovieira77@gmail.com>
2024-01-21 18:09:38 -0300

    menu burguer is working

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,66 +1,32 @@
 import Image from 'next/image';
-import React from 'react';
-
+import React, { useState, useRef } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useRef, useEffect } from 'react';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { useIsomorphicLayoutEffect } from '../helpers/useIsomorphicEffect';
 
 const Nav: React.FC = () => {
-  const menuRef = useRef<HTMLDivElement>(null);
-
-  useIsomorphicLayoutEffect(() => {
-    const menuButton = document.getElementById('menu-button');
-
-    // Função de manipulação do clique
-    const toggleMenu = () => {
-      if (menuRef.current && menuRef.current.classList.contains('responsive')) {
-        menuRef.current.classList.remove('responsive');
-      } else {
-        menuRef.current?.classList.add('responsive');
-      }
-    };
-
-    // Adicionando o event listener, se o menuButton existir
-    if (menuButton) {
-      menuButton.addEventListener('click', toggleMenu);
-    }
-
-    // Cleanup function para remover o event listener
-    return () => {
-      if (menuButton) {
-        menuButton.removeEventListener('click', toggleMenu);
-      }
-    };
-  }, []);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
 
   const isActive = (pathname: string) => {
     return router.pathname === pathname;
   };
 
-  const homeLinkRef = useRef<HTMLAnchorElement>(null);
-  const servicosLinkRef = useRef<HTMLAnchorElement>(null);
-  const contatoLinkRef = useRef<HTMLAnchorElement>(null);
-  const destaqueLinkRef = useRef<HTMLAnchorElement>(null);
-  const portfolioLinkRef = useRef<HTMLAnchorElement>(null);
-
-  const { contextSafe } = useGSAP({});
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <nav className='bg-white shadow-lg relative '>
+    <nav className='bg-white shadow-lg relative'>
       <div className='max-w-6xl mx-auto px-4'>
         <div className='flex justify-between'>
-          <div ref={menuRef} className='md:hidden flex items-center'>
+          <div className='md:hidden flex items-center'>
             <button
               id='menu-button'
               className='outline-none mobile-menu-button z-20'
+              onClick={toggleMenu}
             >
               <svg
-                className='w-6 h-6 text-gray-500 hover:text-green-500'
-                x-show='!showMenu'
+                className='w-6 h-6 text-gray-500 hover:text-red'
                 fill='none'
                 strokeLinecap='round'
                 strokeLinejoin='round'
@@ -97,73 +63,72 @@ const Nav: React.FC = () => {
             </div>
           </div>
 
-          <div className='hidden md:flex items-center space-x-1 z-20 menu'>
+          <div
+            className={`md:flex items-center space-x-1 z-20 menu ${
+              isMenuOpen ? 'responsive' : ''
+            }`}
+          >
             <Link href='/' legacyBehavior>
-              <a
-                ref={homeLinkRef}
-                className={`py-4 px-2 text-gray-500 font-sans font-semibold link ${
-                  isActive('/')
-                    ? 'border-b-2 border-cor-primaria-light'
-                    : 'border-b-2 border-transparent'
-                }`}
-              >
+              <a className='py-4 px-2 text-gray-500 font-sans font-semibold link'>
                 Início
               </a>
             </Link>
 
             <Link href='/servicos' legacyBehavior>
-              <a
-                ref={servicosLinkRef}
-                className={`py-4 px-2 text-gray-500 font-sans font-semibold link ${
-                  isActive('/servicos')
-                    ? 'border-b-2 border-cor-primaria-light'
-                    : 'border-b-2 border-transparent'
-                }`}
-              >
+              <a className='py-4 px-2 text-gray-500 font-sans font-semibold link'>
                 Serviços
               </a>
             </Link>
 
             <Link href='/destaque' legacyBehavior>
-              <a
-                ref={destaqueLinkRef}
-                className={`py-4 px-2 text-gray-500 font-sans font-semibold link ${
-                  isActive('/destaque')
-                    ? 'border-b-2 border-cor-primaria-light'
-                    : 'border-b-2 border-transparent'
-                }`}
-              >
+              <a className='py-4 px-2 text-gray-500 font-sans font-semibold link'>
                 Destaque
               </a>
             </Link>
 
             <Link href='/portfolio' legacyBehavior>
-              <a
-                ref={portfolioLinkRef}
-                className={`py-4 px-2 text-gray-500 font-sans font-semibold link ${
-                  isActive('/portfolio')
-                    ? 'border-b-2 border-cor-primaria-light'
-                    : 'border-b-2 border-transparent'
-                }`}
-              >
+              <a className='py-4 px-2 text-gray-500 font-sans font-semibold link'>
                 Portfólio
               </a>
             </Link>
             <Link href='/contato' legacyBehavior>
-              <a
-                ref={contatoLinkRef}
-                className={`py-4 px-2 text-gray-500 font-sans font-semibold link ${
-                  isActive('/contato')
-                    ? 'border-b-2 border-cor-primaria-light'
-                    : 'border-b-2 border-transparent'
-                }`}
-              >
+              <a className='py-4 px-2 text-gray-500 font-sans font-semibold link'>
                 Contato
               </a>
             </Link>
           </div>
         </div>
       </div>
+
+      {/* <style jsx>{`
+        .responsive {
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          position: absolute;
+          top: 80px;
+          left: 0;
+          background-color: #fff;
+          box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+        }
+
+        @media screen and (max-width: 768px) {
+          .menu {
+            display: none;
+          }
+
+          .responsive {
+            display: block;
+          }
+
+          .responsive a {
+            display: block;
+            text-align: left;
+            padding: 10px 20px;
+            color: red;
+          }
+        }
+      `}</style> */}
     </nav>
   );
 };
