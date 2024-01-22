6f3129655e7e391d358b7221c9f0add51a699b32
wbrunovieira <wbrunovieira77@gmail.com>
2024-01-22 09:20:43 -0300

    clean fews unsused lines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,13 @@ import Home from '../components/Home';
 import LoadingScreen from '../components/LoadingScreen';
 import { useIsomorphicLayoutEffect } from '../helpers/useIsomorphicEffect';
 
-interface HomeProps {
-  isLoaded: boolean;
-}
 function Index() {
   const [isLoading, setIsLoading] = useState(true);
 
   useIsomorphicLayoutEffect(() => {
-    // Simular um carregamento
     setTimeout(() => {
       setIsLoading(false);
-    }, 3000); // Ajuste o tempo conforme necessário
+    }, 1000);
   }, []);
   return <div>{isLoading ? <LoadingScreen /> : <Home />}</div>;
 }
