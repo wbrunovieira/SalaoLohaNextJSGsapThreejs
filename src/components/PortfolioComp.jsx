0251edc9d85fe449872f03af3cfe0bb7555cb116
wbrunovieira <wbrunovieira77@gmail.com>
2024-01-24 14:08:29 -0300

    fixed positions cards in portfolio

--- src/components/PortfolioComp.jsx
+++ src/components/PortfolioComp.jsx
@@ -1,13 +1,12 @@
+import React from 'react';
 import Image from 'next/image';
-import React, { useEffect } from 'react';
 import gsap from 'gsap';
 import { animateTransition } from '../utils/animateTransition';
 import 'lightgallery/css/lightgallery.css';
 import LightGallery from 'lightgallery/react';
-
 import { useIsomorphicLayoutEffect } from '../helpers/useIsomorphicEffect';
 
-const PortfolioComp = () => {
+const PortfolioGrid = () => {
   useIsomorphicLayoutEffect(() => {
     if (typeof window !== 'undefined') {
       import('gsap/TextPlugin').then((module) => {
@@ -33,7 +32,6 @@ const PortfolioComp = () => {
       },
       delay: 3,
     });
-
     return () => {
       gsap.to('html', {
         duration: 1,
@@ -42,49 +40,58 @@ const PortfolioComp = () => {
       });
     };
   }, []);
+  const cards = [
+    { src: '/img/gallery/img1.jpg', title: 'Imagem 1' },
+    { src: '/img/gallery/img2.jpg', title: 'Imagem 2' },
+    { src: '/img/gallery/img3.jpg', title: 'Imagem 3' },
+    { src: '/img/gallery/img4.jpg', title: 'Imagem 3' },
 
+    { src: '/img/gallery/img5.jpg', title: 'Imagem 3' },
+    { src: '/img/gallery/img6.jpg', title: 'Imagem 3' },
+    { src: '/img/gallery/img7.jpg', title: 'Imagem 3' },
+    { src: '/img/gallery/img8.jpg', title: 'Imagem 3' },
+    { src: '/img/gallery/img9.jpg', title: 'Imagem 3' },
+  ];
   const renderImage = (src, idx) => (
     <a key={idx} href={src} data-sub-html={`<h4>Imagem ${idx + 1}</h4>`}>
       <Image width={400} height={300} src={src} alt={`Imagem ${idx + 1}`} />
     </a>
   );
+
   return (
-    <>
-      <div className='transitions'></div>
-      <div className='h-full'>
-        <main className='max-w-6xl mx-auto px-4 font-sans'>
-          <LightGallery
-            mode='lg-fade'
-            speed={600}
-            autoplay={true}
-            autoplayControls={true}
-            pause={5000}
-            download={false}
-            allowMediaOverlap={true}
-            fullScreen
-            showThumbnailWithPlayButton={true}
-          >
-            {[
-              '/img/gallery/img1.jpg',
-              '/img/gallery/img2.jpg',
-              '/img/gallery/img3.jpg',
-              '/img/gallery/img4.jpg',
-              '/img/gallery/img5.jpg',
-              '/img/gallery/img6.jpg',
-              '/img/gallery/img7.jpg',
-              '/img/gallery/img8.jpg',
-              '/img/gallery/img9.jpg',
-              <>
-                <div className='h-full'>
-                  <main className='max-w-6xl mx-auto px-4 font-sans z-20'></main>
-                </div>
-              </>,
-            ].map((src, idx) => renderImage(src, idx))}
-          </LightGallery>
-        </main>
-      </div>
-    </>
+    <div className='container mx-auto p-4'>
+      <LightGallery
+        mode='lg-fade'
+        speed={600}
+        autoplay={true}
+        autoplayControls={true}
+        pause={5000}
+        download={false}
+        allowMediaOverlap={true}
+        fullScreen
+        showThumbnailWithPlayButton={true}
+      >
+        <div className='flex flex-wrap -mx-2'>
+          {cards.map((card, idx) => (
+            <div key={idx} className='px-2 w-full md:w-1/2 lg:w-1/3 mb-4'>
+              <a href={card.src} data-sub-html={`<h4>${card.title}</h4>`}></a>
+              <div className='bg-white shadow-lg rounded-lg overflow-hidden'>
+                <Image
+                  src={card.src}
+                  alt={`Imagem ${idx + 1}`}
+                  width={400}
+                  height={300}
+                  layout='responsive'
+                />
+
+                <a />
+              </div>
+            </div>
+          ))}
+        </div>
+      </LightGallery>
+    </div>
   );
 };
 
-export default PortfolioComp;
+export default PortfolioGrid;
