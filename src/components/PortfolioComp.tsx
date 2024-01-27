b236474bd2c06ca85034bfd946840f206eb0629f
wbrunovieira <wbrunovieira77@gmail.com>
2024-01-27 13:53:31 -0300

    changed photogallery in portfolio

---
+++ src/components/PortfolioComp.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import gsap from 'gsap';
+import { animateTransition } from '../utils/animateTransition';
+import Lightbox from 'yet-another-react-lightbox';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+import 'yet-another-react-lightbox/styles.css';
+import { useIsomorphicLayoutEffect } from '../helpers/useIsomorphicEffect';
+
+import image1 from '/img/gallery/img1.jpg';
+import image2 from '/img/gallery/img2.jpg';
+import image3 from '/img/gallery/img3.jpg';
+import image4 from '/img/gallery/img4.jpg';
+import image5 from '/img/gallery/img5.jpg';
+import image6 from '/img/gallery/img6.jpg';
+import image7 from '/img/gallery/img7.jpg';
+import image8 from '/img/gallery/img8.jpg';
+import image9 from '/img/gallery/img9.jpg';
+
+const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+];
+const imageSizes = [16, 32, 48, 64, 96, 128, 256, 384];
+const deviceSizes = [640, 750, 828, 1080, 1200, 1920, 2048, 3840];
+function nextImageUrl(src: string, size: number) {
+  return `/_next/image?url=${encodeURIComponent(src)}&w=${size}&q=75`;
+}
+const PortfolioComp = () => {
+  const [open, setOpen] = useState(false);
+  useIsomorphicLayoutEffect(() => {
+    animateTransition(true);
+
+    gsap.to('html', {
+      duration: 2,
+      backgroundImage: 'url(/svg/diamond-sunset.svg)',
+      backgroundSize: 'cover',
+      ease: 'power1.in',
+      opacity: 1,
+    });
+
+    gsap.to('p', {
+      top: '40px',
+      ease: 'power4.inOut',
+      duration: 1,
+      stagger: {
+        amount: 0.15,
+      },
+      delay: 3,
+    });
+    return () => {
+      gsap.to('html', {
+        duration: 1,
+        backgroundImage: 'none',
+        ease: 'power1.in',
+      });
+    };
+  }, []);
+  const slides = images.map(({ src, width, height }) => ({
+    width,
+    height,
+    src: nextImageUrl(src, width),
+    srcSet: imageSizes
+      .concat(...deviceSizes)
+      .filter((size) => size <= width)
+      .map((size) => ({
+        src: nextImageUrl(src, size),
+        width: size,
+        height: Math.round((height / width) * size),
+      })),
+  }));
+
+  return (
+    <Lightbox
+      open={open}
+      close={() => setOpen(false)} // Agora 'setOpen' está definido
+      slides={slides}
+      plugins={[Zoom]}
+    />
+  );
+};
+
+export default PortfolioComp;
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,4 +1,4 @@
-import PortfolioComp from '@/components/PortfolioComp.jsx';
+import PortfolioComp from '@/components/PortfolioComp';
 import React from 'react';
 
 const Portfolio: React.FC = () => {
